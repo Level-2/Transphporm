9e75dd4a6c1e39ff852f713d6856c3d2060f354c
Tom Butler <tom@r.je>
2015-11-07 14:03:31 +0000

    more efficient day before tomorrow/day after yesterday inclusion

--- src/Formatter/Date.php
+++ src/Formatter/Date.php
@@ -66,26 +66,23 @@ class Date {
 		return $result;
 	}
 
-	private function ifNull($array, $key) {
-		return isset($array[$key]) ? $array[$key] : null;
-	}
-
 	/** Gets date ranges to represent uses of weeks/months/days/etc */
 	private function getRanges($strings) {
-		return [
+		$ranges =  [
 			[1, 1, $strings['yesterday'], 1, ''],
-			[2, 2, $this->ifNull($strings, 'day_before_yesterday'), 1, ''],
 			[1, 13, $strings['past'], 1, 'days'],
 			[13, 28, $strings['past'], 7, 'weeks'],
 			[28, 365, $strings['past'], 28, 'months'],
 			[365, 99999999, $strings['past'], 365, 'years'],
 			[-1, -1, $strings['tomorrow'], 1, ''],
-			[-2, -2, $this->ifNull($strings, 'day_after_tomorrow'), 1, ''],
 			[-13, -1, $strings['future'], 1, 'days'],
 			[-28, -13, $strings['future'], 7, 'weeks'],
 			[-365, -28, $strings['future'], 28, 'months'],
 			[-999999, -365, $strings['future'], 365, 'years'],
 		];
+		if (isset($strings['day_before_yesterday'])) array_unshift($ranges, [2, 2, $strings['day_before_yesterday'], 1, '']);
+		if (isset($strings['day_after_tomorrow'])) array_unshift($ranges, [-2, -2, $strings['day_after_tomorrow'], 1, '']);
+		return $ranges;
 	}
 
 	/** Converts "week" to "weeks", "month" to "months" etc when plural is required using language from $locale */
@@ -99,7 +96,6 @@ class Date {
 		$strings = $this->locale['offset_strings'];
 
 		foreach ($this->getRanges($strings) as list($lower, $upper, $str, $divisor, $plural)) {
-			if ($str === null) continue;
 			if ($diffDays >= $lower && $diffDays <= $upper) {
 				$num = abs(round($diffDays / $divisor));
 				$result = sprintf($str, $num, $this->getPlural($strings, $num, $plural));				
