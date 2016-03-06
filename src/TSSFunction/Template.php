10d37fbf024ad0d15b7727fbf48122b643e66434
Tom Butler <tom@r.je>
2016-03-06 15:35:06 +0000

    #68 - TSS not supplied to included templates when tss is loaded from a file

--- src/TSSFunction/Template.php
+++ src/TSSFunction/Template.php
@@ -21,9 +21,8 @@ class Template implements \Transphporm\TSSFunction {
 
 	public function run(array $args, \DomElement $element) {
 		$selector = $this->readArray($args, 1);
-		$tss = $this->baseDir . $this->readArray($args, 2);
-		
-		$newTemplate = new \Transphporm\Builder($this->baseDir . $args[0], $tss);
+		$tss = $this->readArray($args, 2);
+		$newTemplate = new \Transphporm\Builder($this->baseDir . $args[0], $tss ? $this->baseDir . $tss : null);
 
 		$doc = $newTemplate->output($this->elementData->getData($element), true)->body;
 		if ($selector != '') return $this->templateSubsection($doc, $selector);
