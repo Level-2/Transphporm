709b88fea2ff00d3ab94209bf63b0a860994a66d
Tom Butler <tom@r.je>
2016-06-13 18:39:29 +0100

    #97 Simplified nl2br

--- src/Formatter/Nl2br.php
+++ src/Formatter/Nl2br.php
@@ -1,37 +1,20 @@
 <?php
 namespace Transphporm\Formatter;
 class Nl2br {
-    public function nl2br($var) {
-        $parts = explode("\n", $var);
-        $doc = new \DomDocument();
-        $root = $doc->createElement('root');
-        $doc->appendChild($root);
-
-        foreach ($parts as $key => $part) {
-            $new = $doc->createTextNode($part);
-            $doc->documentElement->appendChild($new);
-            if ($key !== count($parts)-1) {
-                $br = $doc->createElement('br');
-                $doc->documentElement->appendChild($br);
-            }
-        }
-
-        return $this->getContent($doc);
-    }
-
-    private function getContent($document) {
-		$newNode = $document->documentElement;
+	public function nl2br($var) {
+		$parts = explode("\n", $var);
+		$doc = new \DomDocument();
 		$result = [];
-		if ($newNode->tagName === 'root') {
-			foreach ($newNode->childNodes as $node) {
-				$result[] = $this->getClonedElement($node);
+		
+		foreach ($parts as $key => $part) {
+			$new = $doc->createTextNode($part);
+			$result[] = $new;
+			if ($key !== count($parts)-1) {
+				$br = $doc->createElement('br');
+				$result[] = $br;
 			}
 		}
-		return $result;
-	}
 
-	private function getClonedElement($node) {
-		$clone = $node->cloneNode(true);
-		return $clone;
+		return $result;
 	}
 }
