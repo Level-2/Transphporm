22cc08d746ccf3c9b18eb1e37624578ac2b86f4f
Tom Butler <tom@r.je>
2017-03-13 17:28:15 +0000

    sets basedir in constructor

--- src/Hook/PropertyHook.php
+++ src/Hook/PropertyHook.php
@@ -26,20 +26,18 @@ class PropertyHook implements \Transphporm\Hook {
 		$this->valueParser = $valueParser;
 		$this->pseudoMatcher = $pseudoMatcher;
 		$this->functionSet = $functionSet;
+		if ($this->file !== null) $this->baseDir = dirname(realpath($this->file)) . DIRECTORY_SEPARATOR;
 	}
 
 	public function run(\DomElement $element) {
-		$this->functionSet->setElement($element);
-		if ($this->file !== null) $this->baseDir = dirname(realpath($this->file)) . DIRECTORY_SEPARATOR;
+		$this->functionSet->setElement($element);		
 		$this->configLine = $this->line;
 		try {
 			//Don't run if there's a pseudo element like nth-child() and this element doesn't match it
 			if (!$this->pseudoMatcher->matches($element)) return;
-
 			// TODO: Have all rule values parsed before running them so that things like `content-append` are not expecting tokens
 			// problem with this is that anything in data changed by run properties is not shown
 			// TODO: Allow `update-frequency` to be parsed before it is accessed in rule (might need to switch location of rule check)
-
 			foreach ($this->rules as $name => $value) {
 				$result = $this->callProperty($name, $element, $this->getArgs($value));
 				if ($result === false) break;
