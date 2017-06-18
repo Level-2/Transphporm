ea3e1bfb5d790e0d9ff87283cd43cb09e1ccd4d3
Richard <solleer@hotmail.com>
2017-06-17 21:43:58 -0400

    Resolve #151 - Add file check and alternative for false file contents

--- src/Builder.php
+++ src/Builder.php
@@ -92,9 +92,9 @@ class Builder {
 
 	//Load a template, firstly check if it's a file or a valid string
 	private function loadTemplate() {
-		if (trim($this->template)[0] !== '<') {
+		if (trim($this->template)[0] !== '<' && file_exists($this->template)) {
 			$xml = $this->cache->load($this->template, filemtime($this->template));
-			return $xml ? $xml : ['body' => file_get_contents($this->template), 'headers' => []];
+			return $xml ? $xml : ['body' => file_get_contents($this->template) ?: "", 'headers' => []];
 		}
 		else return ['body' => $this->template, 'headers' => []];
 	}
