338df707e7dbc00a552a8f5ab630c4101695c2a7
Tom Butler <tom@r.je>
2017-06-21 18:41:09 +0100

    delete test file

--- a.php
+++
@@ -1,8 +0,0 @@
-<?php
-$html = '<!doctype html><body>Hello</body>';
-
-
-$doc = new \DomDocument();
-$doc->loadHtml($html,  LIBXML_HTML_NODEFDTD | LIBXML_HTML_NOIMPLIED);
-
-echo $doc->saveXml($doc->doctype);
