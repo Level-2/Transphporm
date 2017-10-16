Level-2/Transphporm

commit d365e4b55d6806191524b82baab5dd74001b9ecd

Fix other issues with removing comments

src/Parser/Tokenizer.php
@@ -73,7 +73,7 @@ class Tokenizer {
 		for ($i = 0; $i < strlen($this->str); $i++) {
 			$char = $this->identifyChar($this->str[$i]);
 			if ($commentChangeI = $this->doComments($tokens, $char, $i)) {
-                $i += $commentChangeI;
+                $i = $commentChangeI;
                 continue;
             }
 
@@ -103,7 +103,7 @@ class Tokenizer {
 	private function doMultiLineComments(&$tokens, $char, $i) {
 		if ($char == Tokenizer::DIVIDE && isset($this->str[$i+1]) && $this->identifyChar($this->str[$i+1]) == Tokenizer::MULTIPLY) {
 			$pos = strpos($this->str, '*/', $i)+2;
-			if ($this->str[$i+$pos] == "\n") $pos++;
+			if ($this->str[$pos] == "\n") $pos++;
 			return $pos ? $pos : 0;
 		}
 	}

tests/TransphpormTest.php
@@ -512,7 +512,7 @@ class TransphpormTest extends PHPUnit_Framework_TestCase {
 		';
 
 		$tss = '/* div {content: "foo"; } */
-		div {content: "bar"} 
+		div {content: "bar"}
 		';
 
 		$template = new \Transphporm\Builder($template, $tss);
@@ -542,7 +542,7 @@ div {content: "bar"; }
 
 		$tss = '
 
-div {// Comment 
+div {// Comment
 	content: "bar"; }
 		';
 
@@ -550,6 +550,22 @@ div {// Comment
 
 		$this->assertEquals('<div>bar</div>', $template->output()->body);
 	}
+
+    public function testComments3() {
+        $template = '
+        <div>foo</div>
+        ';
+
+        $tss = '
+        /* CDN stuff */
+        // Online
+div { content: "bar"; }
+        ';
+
+		$template = new \Transphporm\Builder($template, $tss);
+
+		$this->assertEquals('<div>bar</div>', $template->output()->body);
+    }
 
 	public function testContentTemplate() {
 		$template = '
@@ -1594,13 +1610,13 @@ ul li span {
 		$xml = '<p>My Link</p>';
 
 		$tss = 'p {
-			content: "http://foo/bar";   
+			content: "http://foo/bar";
 		}
 		';
 
 		$template = new \Transphporm\Builder($xml, $tss);
 
-		$this->assertEquals($this->stripTabs('<p>http://foo/bar</p>'), 
+		$this->assertEquals($this->stripTabs('<p>http://foo/bar</p>'),
 			$this->stripTabs($template->output()->body));
 	}
 

commit a0995274b1a5bed31ccae130ed7e29bff41a1ef7

fix bug with multiline comments introduced in d365e4b

src/Parser/Tokenize
@@ -1,0 +1,6 @@
+<?php
+namespace Transphporm\Parser;
+
+interface Tokenize {
+	public function tokenize($str, $tokens, $char, $start);
+}

src/Parser/Tokenizer.php
@@ -72,10 +72,9 @@ class Tokenizer {
 
 		for ($i = 0; $i < strlen($this->str); $i++) {
 			$char = $this->identifyChar($this->str[$i]);
-			if ($commentChangeI = $this->doComments($tokens, $char, $i)) {
-                $i = $commentChangeI;
-                continue;
-            }
+
+            $i += $this->doSingleLineComments($tokens, $char, $i);
+            $i += $this->doMultiLineComments($tokens, $char, $i);
 
 			$this->doNewLine($tokens, $char);
 			$this->doSimpleTokens($tokens, $char);
@@ -88,15 +87,11 @@ class Tokenizer {
 		else return $tokens;
 	}
 
-    private function doComments(&$tokens, $char, $i) {
-        return $this->doSingleLineComments($tokens, $char, $i) +
-               $this->doMultiLineComments($tokens, $char, $i);
-    }
-
 	private function doSingleLineComments(&$tokens, $char, $i) {
 		if ($char == Tokenizer::DIVIDE && isset($this->str[$i+1]) && $this->identifyChar($this->str[$i+1]) == Tokenizer::DIVIDE) {
 			$pos = strpos($this->str, "\n", $i);
-			return $pos ? $pos-2 : 0;
+			if ($pos && $i === 0) return $pos;
+			else if ($pos) return $pos-2;
 		}
 	}
 

src/Parser/Tokenizer/Comments.php
@@ -1,0 +1,6 @@
+<?php
+namespace Transphporm\Parser\Tokenizer;
+
+class Comments implements Transphporm\Parser\Tokenize {
+	
+}