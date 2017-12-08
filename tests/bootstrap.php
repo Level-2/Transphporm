fe8abaafe661f3827b25d5c1a21024934cba7880
Tom Butler <tom@r.je>
2017-12-08 16:56:00 +0000

    update year

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 /* @description     Transformation Style Sheets - Revolutionising PHP templating    *
  * @author          Tom Butler tom@r.je                                             *
- * @copyright       2015 Tom Butler <tom@r.je> | https://r.je/                      *
+ * @copyright       2017 Tom Butler <tom@r.je> | https://r.je/                      *
  * @license         http://www.opensource.org/licenses/bsd-license.php  BSD License *
  * @version         0.9                                                             */
 //Autoloader for Vision classes
