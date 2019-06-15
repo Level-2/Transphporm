Level-2/Transphporm

commit 32f0222fc9760a4a046e712c173c5d377a283dd6

Fixed issue where cached rules would be applied on multiple pages which used different TSS but the same template

src/SheetLoader/TSSFile.php
@@ -64,7 +64,7 @@ class TSSFile implements TSSRules {
 			$this->cacheName = $cacheKey . $this->fileName;
 			return $cacheKey;
 		}
-		else return '';
+		else return $this->fileName;
 	}
 
 	public function getRules($cssToXpath, $valueParser, $sheetLoader, $indexStart) {

commit 8781979c57488f685c9bd820abf6920a8faa62bd

Fix issue introduced in 32f0222fc9760a4a046e712c173c5d377a283dd6 - tests now pass

src/SheetLoader/TSSFile.php
@@ -44,7 +44,7 @@ class TSSFile implements TSSRules {
 	}
 
 	public function updateRequired($data) {
-		$this->cacheName = $this->getCacheKey($data) . $this->fileName;
+		$this->cacheName = $this->getCacheKey($data);
 
 		$rules = $this->getRulesFromCache($this->fileName);
 		//Nothing was cached or the TSS file has changed, update is required