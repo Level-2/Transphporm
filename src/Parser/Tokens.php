6abd387c587e16373305e768d51ccf5d61a4804c
Nathan Bishop <nbish11@hotmail.com>
2022-06-05 14:47:58 +1000

    Add required return types for implemented interfaces

--- src/Parser/Tokens.php
+++ src/Parser/Tokens.php
@@ -13,28 +13,28 @@ class Tokens implements \Iterator, \Countable {
 		$this->tokens = $tokens;
 	}
 
-	public function count() {
+	public function count(): int {
 		return count($this->tokens);
 	}
 
 	// Iterator Functions
-	public function current() {
+	public function current(): mixed {
 		return $this->tokens[$this->iterator];
 	}
 
-	public function key() {
+	public function key(): mixed {
 		return $this->iterator;
 	}
 
-	public function next() {
+	public function next(): void {
 		++$this->iterator;
 	}
 
-	public function valid() {
+	public function valid(): bool {
 		return isset($this->tokens[$this->iterator]);
 	}
 
-	public function rewind() {
+	public function rewind(): void {
 		$this->iterator = 0;
 	}
 
